07e4d901c39cdc8f3ae94447eb420fa81b0fb909
Josimar-Mart <josimar.martins21@gmail.com>
2021-05-07 16:33:10 -0300

    exercicios projeto 1

---
+++ lista-logica/exercicios-Josimar.js
@@ -0,0 +1,111 @@
+// Exemplos
+
+// Exercício 0A
+
+function soma(num1, num2) {
+   // implemente sua lógica aqui
+   return num1 + num2
+}
+
+// Exercício 0B
+
+function imprimeMensagem() {
+   // implemente sua lógica aqui
+   const mensagem = prompt('Digite uma mensagem!')
+
+   console.log(mensagem)
+}
+
+// Exercícios
+
+//Exercício 1
+
+function calculaAreaRetangulo() {
+   // implemente sua lógica aqui
+}
+
+//Exercício 2
+
+function imprimeIdade() {
+   // implemente sua lógica aqui
+}
+
+//Exercício 3
+
+function calculaIMC(peso, altura) {
+   // implemente sua lógica aqui
+
+}
+
+//Exercício 4
+
+function imprimeInformacoesUsuario() {
+   // implemente sua lógica aqui
+
+}
+
+//Exercício 5
+
+function imprimeTresCoresFavoritas() {
+   // implemente sua lógica aqui
+}
+
+//Exercício 6
+
+function retornaStringEmMaiuscula(string) {
+   // implemente sua lógica aqui
+}
+
+//Exercício 7
+
+function calculaIngressosEspetaculo(custo, valorIngresso) {
+   // implemente sua lógica aqui
+}
+
+// Exercício 8
+
+function checaStringsMesmoTamanho(string1, string2) {
+  // implemente sua lógica aqui
+}
+
+// Exercício 9
+
+function retornaPrimeiroElemento(array) {
+   // implemente sua lógica aqui
+}
+
+// Exercício 10
+
+function retornaUltimoElemento(array) {
+   // implemente sua lógica aqui
+}
+
+//Exercício 11
+
+function trocaPrimeiroEUltimo(array) {
+   // implemente sua lógica aqui
+}
+
+// Exercício 12
+
+function checaIgualdadeDesconsiderandoCase(string1, string2) {
+   // implemente sua lógica aqui
+}
+
+// Exercício 13
+
+function checaRenovacaoRG() {
+   // implemente sua lógica aqui
+}
+
+// Exercício 14
+
+function checaAnoBissexto(ano) {
+   // implemente sua lógica aqui
+}
+
+// Exercício 15
+
+function checaValidadeInscricaoLabenu(pessoa) {
+   // implemente sua lógica aqui
+}
